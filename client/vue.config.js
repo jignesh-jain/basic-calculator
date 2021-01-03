d347bbbb1ad96566a7bc5f993bfd3ee2da0555ff
jignesh-jain <jigneshmandot1995@GMAIL.COM>
2021-01-03 11:40:00 -0800

    Added Proxy for Heroku

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer:{
         proxy:{
             '/api': {
-                target: 'http://localhost:5000',
+                target: 'https://still-river-21571.herokuapp.com/',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
