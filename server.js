3f748835c07bfcc4b2e53516d3d31b0bc32b50c0
jignesh-jain <jigneshmandot1995@GMAIL.COM>
2021-01-03 09:17:35 -0800

    Ready for Deployment

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose
 app.use(cors());
 
 // Stattic Folder Middleware
-app.use(express.static("public"))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 // Get Recent 10 Calculations
 
@@ -77,10 +77,9 @@ io.on('connection', (socket) => {
 
   if (process.env.NODE_ENV === "production") {
     // Set Static Folder
-    app.use(express.static("client/build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
+    app.get(/.*/, function (req, res) {
+      res.sendFile(path.join(__dirname, '/dist/index.html'))
+    })
   }
 
 server.listen(PORT, () => {
